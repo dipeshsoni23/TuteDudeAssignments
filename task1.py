39067ba125162040616a83537eb98477ccd4a02f
dipeshsoni23 <dipesh.23jdaiml040@jietjodhpur.ac.in>
2025-09-27 06:37:40 +0530

    Commit & Push

--- task1.py
+++ task1.py
@@ -5,7 +5,7 @@ add=num1+num2
 sub=num1-num2
 mul=num1*num2
 div=num1/num2
-print("Addition:",add)
-print("Substraction:",sub)
-print("Multiplication:",mul)
-print("Division",div)
+print("Addition: ",add)
+print("Substraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
